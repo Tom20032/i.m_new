ccab4294e0a8e45132120b0f5160f0e459be2772
Tom20032 <141570704+Tom20032@users.noreply.github.com>
2024-05-09 16:45:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,23 @@
-const min = document.getElementById("min");
-const sec = document.getElementById("sec");
-let a = 0;
+const count1 = document.getElementById("count");
 
 
 
-document.getElementById("reset").onclick = function (){
-   
-    sec.textContent = "00";
-    a=a+1;
-    setTimeout(function(){
-        let secone = parseInt(sec.textContent);
-        sec.textContent = secone - 1;
-        a=a-1;
-    }, 1000)
+document.getElementById("up").onclick = function(){
+    let counter = parseInt(count1.textContent);
+    count1.textContent = counter + 1;
+    console.log("it wroks up")
 }
 
-document.getElementById("stop").onclick = function (){
-   
-    
-    a=a+1;
-    setTimeout(function(){
-        let secone = parseInt(sec.textContent);
-        sec.textContent = secone - 1;
-       
-    }, 1000)
-    if(sec.textContent===-1){
-    let secone = parseInt(sec.textContent);
-    sec.textContent = secone + 1;
-    if(sec.textContent===-1){
-    let secone = parseInt(sec.textContent);
-    sec.textContent = secone + 1;
-}
-}
+document.getElementById("down").onclick = function(){
+    let counter = parseInt(count1.textContent);
+    count1.textContent = counter - 1;
+    console.log("it wroks down")
 }
 
-document.getElementById("start").onclick = function (){
-    
-    function loop(){
-        setTimeout(function(){
-        
-        let secone = parseInt(sec.textContent); // Haal de huidige waarde op en converteer naar een getal
-        sec.textContent = secone + 1;
-        if(sec.textContent < 60 && a===0) {
-            loop();
-        }
-    }, 1000)
-}
-     loop();
+document.getElementById("reset").onclick = function(){
+    let counter = parseInt(count1.textContent);
+    count1.textContent = "0";
+    /* alert("reset to 0"); */
 }
 
 
@@ -59,59 +30,6 @@ document.getElementById("start").onclick = function (){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-document.getElementById("reset").onclick = function (){
-    sec.textContent = "00";
-}
-
-document.getElementById("start").onclick = function (){
-    
-    function loop(){
-        setTimeout(function(){
-        
-        let secone = parseInt(sec.textContent); // Haal de huidige waarde op en converteer naar een getal
-        sec.textContent = secone + 1;
-        if(sec.textContent < 60) {
-            loop();
-        }
-    }, 1000)
-}
-    loop();
-}
-
-document.getElementById("stop").onclick = function (){
-    let secone = parseInt(sec.textContent); // Haal de huidige waarde op en converteer naar een getal
-    sec.textContent = secone + 1;
-}
-
-
-
-*/
 
 
 
